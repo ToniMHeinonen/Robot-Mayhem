2f15f568c13d4b4dd5e6734dd0fe7e1aa86b2542
Toni <toni.heinonen@hotmail.fi>
2019-03-04 16:32:21 +0200

    Initial commit

---
+++ desktop/src/fi/tamk/fi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tamk.fi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tamk.fi.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
