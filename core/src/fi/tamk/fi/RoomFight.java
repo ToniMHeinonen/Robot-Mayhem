8f07b94a905f60cc249ebe6e2a2b0b580d241cee
LJulkunen <laura.julkunen@tuni.fi>
2019-03-06 09:05:27 +0200

    Removed InputProcessor & unnecessary methods that came with it.

--- core/src/fi/tamk/fi/RoomFight.java
+++ core/src/fi/tamk/fi/RoomFight.java
@@ -18,7 +18,7 @@ import static javax.swing.text.html.HTML.Attribute.ROWS;
 
 // I comment at least on things that are unclear to me.
 // I comment out lines of code that aren't functional yet.
-public class RoomFight extends RoomParent implements InputProcessor {
+public class RoomFight extends RoomParent {
 
     // Not sure what I should put in this constructor.
     RoomFight(MainGame game) {
@@ -92,45 +92,5 @@ public class RoomFight extends RoomParent implements InputProcessor {
         //update();
 
         batch.end();
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
     }
 }
