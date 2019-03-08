357bb5678bafaabfbb074302ebb2a9e5d0b342ce
Toni <toni.heinonen@hotmail.fi>
2019-03-08 09:08:03 +0200

    Modified simpler and more efficient code for buttons, no need to use boolean values. Also added button to switch to game room.

--- core/src/fi/tamk/fi/RoomSettings.java
+++ core/src/fi/tamk/fi/RoomSettings.java
@@ -5,11 +5,16 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class RoomSettings extends RoomParent {
-    private boolean moveToFightRoom;
 
     RoomSettings(MainGame game) {
         super(game);
         createButtonFight();
+        createButtonGame();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
     }
 
     public void createButtonFight() {
@@ -23,21 +28,26 @@ public class RoomSettings extends RoomParent {
         buttonFight.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y){
-                moveToFightRoom = true;
+                RoomFight roomFight = new RoomFight(game);
+                game.setScreen(roomFight);
             }
         });
     }
 
-    public void checkPresses() {
-        if (moveToFightRoom) {
-            RoomFight roomFight = new RoomFight(game);
-            game.setScreen(roomFight);
-        }
-    }
+    public void createButtonGame() {
+        final TextButton buttonFight = new TextButton("RoomGame", skin);
+        buttonFight.setWidth(300f);
+        buttonFight.setHeight(100f);
+        buttonFight.setPosition(game.pixelWidth /2 - buttonFight.getWidth() /2,
+                game.pixelHeight/3 - buttonFight.getHeight() /2);
+        stage.addActor(buttonFight);
 
-    @Override
-    public void render(float delta) {
-        super.render(delta);
-        checkPresses();
+        buttonFight.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                RoomGame room = new RoomGame(game);
+                game.setScreen(room);
+            }
+        });
     }
 }
