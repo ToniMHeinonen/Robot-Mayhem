58ce4d6cc22096643252bca2952e7a6e8cb90764
OAhonen <olli.p.ahonen@tuni.fi>
2019-03-09 08:01:58 +0200

    Moved CreateSkin and CreateStage from parentroom to maingame. Changed stage's StretchViewport to FitViewport.

--- core/src/fi/tamk/fi/MainGame.java
+++ core/src/fi/tamk/fi/MainGame.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MainGame extends Game {
 	private SpriteBatch batch;
@@ -16,12 +19,22 @@ public class MainGame extends Game {
 	public static final float pixelWidth = 1920f;
 	public static final float pixelHeight = 1080f;
 	private OrthographicCamera camera;
-	
+
+	private Stage stage;
+	private Skin skin;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, pixelWidth, pixelHeight);
+		stage = new Stage(new FitViewport(pixelWidth, pixelHeight), batch);
+		// Skin: https://github.com/czyzby/gdx-skins/tree/master/glassy
+		// Check "License" bottom of the page
+		// Files in \android\assets:
+		// font-big-export.fnt, font-export.fnt, glassy-ui.atlas,
+		// glassy-ui.json, glassy-ui.png
+		skin = new Skin( Gdx.files.internal("glassy-ui.json") );
 
 		loadTextures();
 
@@ -34,11 +47,13 @@ public class MainGame extends Game {
 	public void render () {
 		super.render();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		gamePlayer.dispose();
+		stage.dispose();
+		skin.dispose();
 	}
 
 	public void loadTextures() {
@@ -49,11 +64,19 @@ public class MainGame extends Game {
 		return batch;
 	}
 
-    public OrthographicCamera getCamera() {
-        return camera;
-    }
+	public OrthographicCamera getCamera() {
+		return camera;
+	}
 
 	public Texture getGamePlayer() {
 		return gamePlayer;
+	}
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public Skin getSkin() {
+		return skin;
 	}
 }
