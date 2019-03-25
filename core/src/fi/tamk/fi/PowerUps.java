0c8ce8ee458cc70178c39ccc2bfec4d39829642f
LJulkunen <laura.julkunen@tuni.fi>
2019-03-25 17:30:51 +0200

    Three powerups are generated randomly. The first one is 1-3, second 4-6 and third one is 7-9. They are also added to an arraylist.

--- core/src/fi/tamk/fi/PowerUps.java
+++ core/src/fi/tamk/fi/PowerUps.java
@@ -4,13 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import java.awt.Rectangle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 
 public class PowerUps extends RoomParent {
 
     SpriteBatch batch;
     //Rectangle rectangle;
     Texture box;
+    int pupA;
+    int pupB;
+    int pupC;
+
+    Array<Integer> pups;
 
     PowerUps(MainGame game) {
         super(game);
@@ -23,6 +29,27 @@ public class PowerUps extends RoomParent {
         box = new Texture(Gdx.files.internal("box.png"));
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920, 1080);
+
+        pups = new Array<Integer>();
+
+        for (int i = 0; i < 1; i++) {
+
+            pupA = MathUtils.random(1, 3);
+            pups.add(pupA);
+        }
+
+        for (int i = 0; i < 1; i++) {
+
+            pupB = MathUtils.random(4, 6);
+            pups.add(pupB);
+        }
+
+        for (int i = 0; i < 1; i++) {
+
+            pupC = MathUtils.random(7, 9);
+            pups.add(pupC);
+            System.out.println(pups);
+        }
     }
 
     public void render (float delta) {
